6ac2e4eeb541744d862274bce12f28797cbe0449
Hailegebriel22 <eartalea@gmail.com>
2022-11-11 00:09:02 +0100

    App route

--- src/client/App.js
+++ src/client/App.js
@@ -4,43 +4,23 @@ import TestComponent from "./components/TestComponent/TestComponent";
 import MealsWithID from "./components/TestComponent/MealsWithID";
 import Home from "./components/TestComponent/Home";
 import Meals from './components/TestComponent/Meals';
-import Review from "./components/TestComponent/Review"
+import Review from "./components/TestComponent/Review";
 import { useState } from "react";
 import "./App.css";
 import AddMeal from "./components/TestComponent/AddMeal";
-//import NavBar from "./components/TestComponent/NavBar";
+
 function App() {
   const [meals, setMeals] = useState([]);
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
 
-
-  return (
-    // <Router>
-    
+  return (    
     <Routes>
-                <Route exact path="/" element={<Home/>} />  
-               
-
-        {/* //<NavBar /> */}
-        {/* <Route exact path="/meals" element={<TestComponent />} /> */}
-
-      
-
-      {/* <Route exact path="/meals/:id">
-          <MealsWithID meals={meals} setMeals={setMeals} />
-        </Route> */}
-      {/*<Route exact path="/test-component">
-        <TestComponent></TestComponent>
-
-      </Route> */}
-      <Route exact path="/meals" element={<Meals meals={meals} setMeals={setMeals}/>}/> 
-
-      <Route exact path="/meals/:id" element={<MealsWithID meals={meals} setMeals={setMeals}/>} />
-      <Route exact path="/meals/add-meal" element={<AddMeal meals={meals} setMeals={setMeals}/>} />
-      <Route exact path="/reviews" element={<Review reviews={reviews} setReviews={reviews}/>} />
-
-    </Routes>
-    // </Router>
+     <Route exact path="/" element={<Home/>} />              
+     <Route exact path="/meals" element={<Meals meals={meals} setMeals={setMeals}/>}/> 
+     <Route exact path="/meals/:id" element={<MealsWithID meals={meals} setMeals={setMeals}/>} />
+     <Route exact path="/meals/add-meal" element={<AddMeal meals={meals} setMeals={setMeals}/>} />
+     <Route exact path="/reviews" element={<Review reviews={reviews} setReviews={setReviews}/>} />
+   </Routes>
   );
 }
 
